da740dea6f9a8f4ede1599a420d81325df3b9a90
이설 <sn0w02@naver.com>
2026-09-01 17:46:21 +0900

    메인페이지 통합작업 중간저장

--- js/section main-visual.js
+++ js/section main-visual.js
@@ -1,23 +1,23 @@
-<!-- Swiper JS -->
+//< !--Swiper JS-- >
     <script src="https://cdn.jsdelivr.net/npm/swiper@14.0.1/swiper-bundle.min.js"></script>
 
-    <!-- Initialize Swiper -->
+//<!--Initialize Swiper-- >
     <script>
         const progressCircle = document.querySelector('.autoplay-progress svg');
         const progressContent = document.querySelector('.autoplay-progress span');
         var swiper = new Swiper('.mySwiper', {
             spaceBetween: 30,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
+        pagination: {
+            el: '.swiper-pagination',
+        clickable: true,
             },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+        navigation: {
+            nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
             },
-            on: {
-                slideChange: function () {
-                    playCurrentVideo(this);
+        on: {
+            slideChange: function () {
+            playCurrentVideo(this);
                 }
             },
         });
@@ -25,20 +25,20 @@
         function playCurrentVideo(swiperInstance) {
             const allVideos = document.querySelectorAll('.swiper-slide video');
             allVideos.forEach(video => {
-                video.pause();
-                video.currentTime = 0;
-                video.onended = null;
+            video.pause();
+        video.currentTime = 0;
+        video.onended = null;
             });
 
-            const activeSlide = swiperInstance.slides[swiperInstance.activeIndex];
-            const currentVideo = activeSlide.querySelector('video');
+        const activeSlide = swiperInstance.slides[swiperInstance.activeIndex];
+        const currentVideo = activeSlide.querySelector('video');
 
-            if (currentVideo) {
-                currentVideo.play().catch(error => {
-                    console.log(error);
-                });
-                currentVideo.onended = function () {
-                    swiperInstance.slideNext();
+        if (currentVideo) {
+            currentVideo.play().catch(error => {
+                console.log(error);
+            });
+        currentVideo.onended = function () {
+            swiperInstance.slideNext();
                 };
             }
         }
